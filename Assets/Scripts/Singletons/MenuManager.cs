687a9dc24911560100b9de701a684cea331d4334
Baker <eab6863@gmail.com>
2020-04-02 13:09:11 -0400

    Added MenuManager

---
+++ Assets/Scripts/Singletons/MenuManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MenuManager : Singleton<MenuManager>
+{
+    // (Optional) Prevent non-singleton constructor use.
+    protected MenuManager() { }
+
+    private void Awake()
+    {
+        
+    }
+}
