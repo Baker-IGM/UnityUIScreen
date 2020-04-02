9a7690a701664ebb8e0e70c492351d946cad7e57
Baker <eab6863@gmail.com>
2020-04-02 13:39:03 -0400

    Setup new Input System

--- Assets/Scripts/Singletons/MenuManager.cs
+++ Assets/Scripts/Singletons/MenuManager.cs
@@ -1,12 +1,91 @@
 ﻿using UnityEngine;
 
+public enum MenuStates
+{
+    Main,
+    Game,
+    Pause,
+    LevelSelect
+}
+
 public class MenuManager : Singleton<MenuManager>
 {
+    [SerializeField]
+    MenuStates startState;
+
+    MenuStates currentMenuState;
+
     // (Optional) Prevent non-singleton constructor use.
     protected MenuManager() { }
 
     private void Awake()
     {
-        
+        ChangeMenuState(startState);
+    }
+
+    void ChangeMenuState(MenuStates newMenuState)
+    {
+        switch(newMenuState)
+        {
+            case MenuStates.Main:
+            case MenuStates.LevelSelect:
+            case MenuStates.Game:
+                SetAllMenusTo(false);
+
+                SetMenuTo(newMenuState, true);
+
+                currentMenuState = newMenuState;
+                break;
+            case MenuStates.Pause:
+                if(currentMenuState == MenuStates.Game)
+                {
+                    SetMenuTo(newMenuState, true);
+
+                    currentMenuState = newMenuState;
+                }
+                else if(currentMenuState == MenuStates.Pause)
+                {
+                    SetMenuTo(newMenuState, false);
+
+                    currentMenuState = MenuStates.Game;
+                }
+                break;
+        }
+    }
+
+    void SetAllMenusTo(bool isActive)
+    {
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            transform.GetChild(i).gameObject.SetActive(isActive);
+        }
+    }
+
+    void SetMenuTo(MenuStates menu, bool isActive)
+    {
+        if((int)menu < transform.childCount)
+        {
+            transform.GetChild((int)menu).gameObject.SetActive(isActive);
+        }
+    }
+
+    public void OnGoToMain()
+    {
+        ChangeMenuState(MenuStates.Main);
+    }
+
+    public void OnGoToGame()
+    {
+        ChangeMenuState(MenuStates.Game);
+    }
+
+    public void OnGoToLevelSelect()
+    {
+        ChangeMenuState(MenuStates.LevelSelect);
+    }
+
+    public void OnPause()
+    {
+        ChangeMenuState(MenuStates.Pause);
     }
 }
